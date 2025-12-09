440604410983285d800143c4df7fead553c50125
Julia Samól <j.samol@papers.ch>
2025-12-09 10:19:42 +0100

    chore(mobile-node): version 2.0.39-beta03

--- mobile-node/android/app/build.gradle.kts
+++ mobile-node/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 object Library {
     const val groupId = "com.github.smoldot"
     const val artifactId = "smoldot-android"
-    const val version = "2.0.39-beta02"
+    const val version = "2.0.39-beta03"
 }
 
 android {
